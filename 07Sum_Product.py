d82fd87525f8a583282d3f84b0303cf11e01aa0a
jleong1msudenver <jleong1@msudenver.edu>
2021-02-04 15:12:45 -0700

    assigned variables, addition and product calculations working

--- 07Sum_Product.py
+++ 07Sum_Product.py
@@ -1,9 +1,20 @@
 # 07 Sum/Product Program
 # Due Thursday by 11:59pm Points 5 Submitting a file upload
 # Write a Python program that reads two integers from the user, 
-# calculates the sum and product of the two umbers, and prints 
+# calculates the sum and product of the two numbers, and prints
 # them on one line with messages identifying each calculated number.
 
-# [ ] Input two integers from user
+# [X] Input two integers from user
 # [ ] calculates sum and product of two numbers
-# [ ] print on one line with messages idenfifying calculated number.
+# [ ] print on one line with messages identifying calculated number.
+
+# Capture input for first typed input
+integer1 = int(input("Enter an integer:"))
+# Capture input from second typed input
+integer2 = int(input("Enter a second integer:"))
+# Add inputs together to equal sum
+sum = integer1 + integer2
+product = integer1 * integer2
+print(sum)
+print(product)
+print('The sum of', integer1, '+', integer2, 'equals:', sum)
