07fad376d255bbda92b770fbafa0b63d62cdbc8e
jleong1msudenver <jleong1@msudenver.edu>
2021-02-03 20:59:42 -0700

    added assignment instructions

---
+++ 07Sum_Product.py
@@ -0,0 +1,6 @@
+# 07 Sum/Product Program
+# Due Thursday by 11:59pm Points 5 Submitting a file upload
+# Write a Python program that reads two integers from the user, 
+# calculates the sum and product of the two umbers, and prints 
+# them on one line with messages identifying each calculated number.
+
