55b0a8ce7d65853d6408001fc90cb4956b501e38
jleong1msudenver <jleong1@msudenver.edu>
2021-02-08 19:51:49 -0700

    added instructions for String Assignment

---
+++ JoshLeong_4_String.py
@@ -0,0 +1,7 @@
+# Take a string as input for a name and a string for course number and outputs a welcome message.
+# For example: if the input name is “Jane” and the course name is “CS 1030”, the output is: “Welcome, Jane, to course CS 1030!” 
+
+# Take a string as input, say input_string, then print the first 2 and the last 2 characters in the string. 
+#  Assume the input string is 2 or more characters. The print should look like:
+#   “The first 2 characters are: ”the first 2
+#   “The last  2 characters are: ” the last 2
